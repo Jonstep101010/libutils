88193ad0f3e1ad8ff8e3a44126ec5ae6c733714f
jschwabe <jschwabe@student.42.fr>
2023-07-14 16:39:11 +0200

    mod printf size_t

--- src/printf/ft_printf.c
+++ src/printf/ft_printf.c
@@ -6,26 +6,21 @@
 /*   By: jschwabe <jschwabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 13:08:13 by jschwabe          #+#    #+#             */
-/*   Updated: 2023/05/31 19:23:36 by jschwabe         ###   ########.fr       */
+/*   Updated: 2023/07/14 16:36:11 by jschwabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	put_char(char c);
-int	put_str(char *str);
-int	put_nbr(size_t n, char *base, size_t slen);
-
-/*
-@brief calls putnbr with specific parameters
-@param n maintains org val
-@return length returned by put_nbr call (-1 if failure)
-*/
-static int	format_u(long n)
+static int	format_zu(size_t n, int *size)
 {
-	if (n < 0)
-		return (put_nbr((size_t)UINT_FAST32_MAX + n + 1, "0123456789", 10));
-	return (put_nbr((size_t)n, "0123456789", 10));
+	int	check;
+
+	check = put_nbr(n, "0123456789", 10);
+	if (check < 0)
+		return (FAIL);
+	*size += check;
+	return (check);
 }
 
 /*
@@ -77,7 +72,7 @@ static int	check_formatter(va_list args, int specifier, int *size)
 	if ((specifier == 'd') || (specifier == 'i'))
 		check = format_di((long)va_arg(args, int));
 	if (specifier == 'u')
-		check = format_u((long)va_arg(args, unsigned int));
+		check = put_nbr(va_arg(args, unsigned int), "0123456789", 10);
 	if (specifier == 'x')
 		check = put_nbr(va_arg(args, unsigned int), "0123456789abcdef", 16);
 	if (specifier == 'X')
@@ -103,8 +98,14 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (*format)
 	{
-		if (*format == MARKER)
-		{		
+		if (*format == MARKER && *(format + 1) == 'z' && (*(format + 2) == 'u'))
+		{
+			if (format_zu(va_arg(args, size_t), &size) < 0)
+				return (-1);
+			format += 2;
+		}
+		else if (*format == MARKER)
+		{
 			if (*(format + 1) && check_formatter(args, *(++format), &size) < 0)
 				return (-1);
 		}
