a88ec28f47a973cc26f1d9b1c24bb128c1d7a937
jschwabe <mail@jschwabe.site>
2024-01-31 14:43:12 +0000

    Add -g3 flag to clang release compiler and fix arr_free null check
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/arr/arr_free.c
+++ src/arr/arr_free.c
@@ -6,7 +6,7 @@
 /*   By: jschwabe <jschwabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 20:07:56 by jschwabe          #+#    #+#             */
-/*   Updated: 2023/07/25 16:03:52 by jschwabe         ###   ########.fr       */
+/*   Updated: 2024/01/31 14:42:41 by jschwabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	arr_free(char **arr)
 	int	i;
 
 	i = -1;
-	while (arr[++i])
+	while (arr && arr[++i])
 	{
 		free(arr[i]);
 		arr[i] = NULL;
